a1d61ff0c0d0147869a92d96497e1d4e3da357d7
wojtas99 <wojciechkraw99@gmail.com>
2026-01-01 16:40:25 +0100

    All is finished

--- src/EasyBot_DLL/BotCore/BuildConfig.h
+++ src/EasyBot_DLL/BotCore/BuildConfig.h
@@ -26,7 +26,6 @@ enum {
 #define BUILD_OUTCAST 4
 #define BUILD_THIRUS  5
 #define BUILD_EXORDION  6
-#define BUILD_DBL  7
 
 
 #define BuildOption BUILD_MIRACLE
@@ -61,12 +60,6 @@ enum {
     #define singletonFunctionOffset 0x10
     #define globalFieldOffset 0x10
 #elif BuildOption == BUILD_EXORDION
-    static const BYTE* callGlobalField_PATTERN = reinterpret_cast<const BYTE*>("\x55\x8b\xec\x8b\x45\x00\x83\x78\x00\x00\x00\x00\x8b\x00\x50\x68\x00\x00\x00\x00\xff\x35\x00\x00\x00\x00\xe8\x00\x00\x00\x00\x6a");
-    static LPCSTR callGlobalField_MASK = "xxxxx?xx????xxxx????xx????x????x";
-    #define classFunctionOffset 0x58
-    #define singletonFunctionOffset 0x10
-    #define globalFieldOffset 0x10
-#elif BuildOption == BUILD_DBL
     static const BYTE* callGlobalField_PATTERN = reinterpret_cast<const BYTE*>("\x55\x8b\xec\x8b\x45\x00\x83\x78\x00\x00\x00\x00\x8b\x00\x50\x68\x00\x00\x00\x00\xff\x35\x00\x00\x00\x00\xe8\x00\x00\x00\x00\x6a");
     static LPCSTR callGlobalField_MASK = "xxxxx?xx????xxxx????xx????x????x";
     #define classFunctionOffset 0x58
